66ed57f9971f283edab1db86c9d087ce158a22c1
julianmackenzie <113747039+julianmackenzie@users.noreply.github.com>
2023-10-17 19:54:56 -0500

    Added throwable text and more prompts

--- CAVE2/Assets/DizzyTrigger.cs
+++ CAVE2/Assets/DizzyTrigger.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DizzyTrigger : MonoBehaviour
 {
+    public GameObject overwhelmedPromptText;
+    public GameObject dizzyPromptText;
+
 
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Player") {
             EventManager.OnDizzyTrigger();
+
+            overwhelmedPromptText.SetActive(false);
+            dizzyPromptText.SetActive(true);
+
         }
     }
 
