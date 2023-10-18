4f38a912cc4a7a4d276a8d04c3e571d2d3fc0467
rwisni2 <rwisni2@uic.edu>
2023-10-17 22:36:04 -0500

    added speech particles

---
+++ CAVE2/Assets/SpeechParticles.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextParticleEmitter : MonoBehaviour
+{
+    public Text uiText;
+    
+    void Start()
+    {
+        uiText = GetComponent<Text>();
+        uiText.enabled = false; // Initially hide the UI Text
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Adjust the condition as needed
+        {
+            EmitTextParticle();
+        }
+    }
+
+    void EmitTextParticle()
+    {
+        uiText.enabled = true; // Show the UI Text
+        uiText.text = "Your Text Here"; // Set the text on the UI Text
+
+        // Implement a logic to hide the UI Text after a certain duration or condition.
+    }
+}
