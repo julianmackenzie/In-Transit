61a802ccc9b87fe9834da1d3ae824ecd1d25693d
rwisni2 <rwisni2@uic.edu>
2023-10-23 11:33:17 -0500

    sound update + particleToggle

---
+++ CAVE2/Assets/playSoundOnEnter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSoundOnEnter : MonoBehaviour
+{
+    public AudioClip mySound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            AudioSource audioSource = GetComponent<AudioSource>();
+            audioSource.PlayOneShot(mySound);
+        }
+    }
+}
