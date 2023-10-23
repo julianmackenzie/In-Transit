bba716b9ff8e8259c536cc4e21a251a38e7fc251
julianmackenzie <113747039+julianmackenzie@users.noreply.github.com>
2023-10-23 00:32:18 -0500

    Sit prompt and REFRAME label added

---
+++ CAVE2/Assets/SitPromptTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SitPromptTrigger : MonoBehaviour
+{
+
+    [SerializeField] GameObject dizzyPromptText;
+    [SerializeField] GameObject sitPromptText;
+
+    // Start is called before the first frame update
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+
+            dizzyPromptText.SetActive(false);
+            sitPromptText.SetActive(true);
+
+        }
+
+    }
+
+}
