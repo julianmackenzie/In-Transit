d94e9b6cdea627a8953ab9a0173a0cc71e117215
rwisni2 <rwisni2@uic.edu>
2023-10-28 02:08:16 -0500

    FIXED SOUND
    Added heartbeat, subway, train sound effect on the first platform scene. In the 2nd train scene, added heartbeat, heavy breathing, inside train sound. Added correct trigger zones for crowd and whoosh

---
+++ CAVE2/Assets/CrowdChatterScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdChatterScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private AudioSource audioSource; // added whoosh audio
+
+    private void Start()
+    {
+        // Get the AudioSource component from this GameObject
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+
+            if (audioSource != null && audioSource.clip != null)
+            {
+                audioSource.Play();
+            }
+
+        }
+    }
+}
--- CAVE2/Assets/DizzyTrigger.cs
+++ CAVE2/Assets/DizzyTrigger.cs
@@ -8,6 +8,14 @@ public class DizzyTrigger : MonoBehaviour
     public GameObject overwhelmedPromptText;
     public GameObject dizzyPromptText;
 
+    private AudioSource audioSource; // added whoosh audio
+
+    private void Start()
+    {
+        // Get the AudioSource component from this GameObject
+        audioSource = GetComponent<AudioSource>();
+    }
+
 
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Player") {
@@ -15,6 +23,11 @@ public class DizzyTrigger : MonoBehaviour
 
             overwhelmedPromptText.SetActive(false);
             dizzyPromptText.SetActive(true);
+
+            if (audioSource != null && audioSource.clip != null)
+            {
+                audioSource.Play();
+            }
 
         }
     }
